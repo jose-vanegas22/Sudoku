4e11cae3d652280a9071772c8c8fb0a3a0fc05de
Alejandro Medina <weaqla.xd@gmail.com>
2025-04-07 15:06:26 -0500

    creation of a test app, slightly modifications to board.

--- src/main/java/com/example/juegosudoku/TestMain.java
+++ src/main/java/com/example/juegosudoku/TestMain.java
@@ -1,6 +1,12 @@
 package com.example.juegosudoku;
 
+import com.example.juegosudoku.models.Board;
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class TestMain extends Application {
@@ -11,6 +17,21 @@ public class TestMain extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        Stage testStage = new Stage();
+        testStage.setTitle("Test");
+        Label title = new Label("Test");
+        Button button = new Button("Test board gen");
+        button.setOnAction(e -> {
+            Board board = new Board();
+            board.generateNumber(6);
+        });
+        VBox root = new VBox();
+        root.getChildren().addAll(title, button);
+        testStage.setResizable(false);
+
+        Scene scene = new Scene(root);
+        testStage.setScene(scene);
+        testStage.show();
 
     }
 }
--- src/main/java/com/example/juegosudoku/models/Board.java
+++ src/main/java/com/example/juegosudoku/models/Board.java
@@ -6,23 +6,31 @@ public class Board {
     private static final int boardWidth=6;
 
     public Board() {
-        board=new int[boardLength][boardWidth];
+
+        board=new int[boardLength][boardWidth] = null;
     }
 
-    public void generateNumber(){
+    public void generateNumber(int i) {
         int fila = (int) (Math.random() * 6) + 1;
         int columna = (int) (Math.random() * 6) + 1;
         int number = (int) (Math.random() * 6) + 1;
-        if(checkEmprtyCell(fila, columna))
+        if(i == 0)
         {
-            board[fila][columna] = number;
-        }
-        else {
-            generateNumber();
+            System.out.println(board);
+        } else if (checkEmptyCell(columna, fila) == false) {
+            System.out.println("La casilla esta ocupada");
+
+        } else {
+            board[columna][fila] = number;
+            generateNumber(i-1);
         }
     }
 
-    public boolean checkEmprtyCell(int fila, int columna){
-        return board[fila][columna]==0;
+    public boolean checkEmptyCell(int fila, int columna){
+        if (board[columna][fila] == null;){
+            return true;
+        }
     }
+
+
 }
